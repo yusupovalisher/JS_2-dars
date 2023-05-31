7b67fac832602118ac9c0d9d251b66be3369094f
yusupovalisher <volk8489@gmail.com>
2023-05-31 12:59:38 +0500

    Bu  JS 2-dars kodlari

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/*let a = 44;
+let b = 43;
+
+if (a >= b){
+    console.log("a qiymati 44dan katta yoki teng")
+}else{
+    console.log("a ning qiymati ko'rsatilgan songa teng emas!")
+}*/
+/*let a = 10;
+let b = 46;
+let c = 50;
+let d = 55;*/
+
+/*if (a == 45 && b == 30 && c == 12 && d == 41){
+    console.log("Shartlarni hammasi bajarilsa bajarilda")
+}
+else{
+    console.log("Shartning kamida bittasi ishlamayapti")
+}*/
+/*if (a == 40 || b == 35 || c == 23 || d == 14){
+    console.log("Shartlrning kamida bittasi bajarilgan")
+}
+else {
+    console.log(" Shartlardan hammasi bajarilmaydi")
+}*/
+/* JS data tepes: undefined,booleon,object*/
+
+/*let soz = "text";
+let son = 45;
+let matn;*/
+/*let rost = 45==45?true:false;*/
+/*if (45==45){
+    document.write(true)
+}
+else {
+    document.write(false)
+}*/
+
+let arrayObject = [45,78,"text","matn",true,false];
+
+let oddiyObject = {ismi:"",yoshi:18,talabami: "yoq"};
+let poytaxt = "Toshkent";
+if (oddiyObject. aholi >36){
+     poytaxt = "Toshkent emas"
+}
+
+console.log("Toshkent" + poytaxt);
